a751c1f9e9e2e39c3edfdaaa1d6978b779ae5491
harsha040698 <iamharshat@gmail.com>
2026-01-26 18:13:41 -0500

    "Day1: bandnamegenerator program"

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.\n")
+cityname=input("What's the name of the city you grew up in?\n")
+petname=input("What's your pet name?\n")
+print("Your band name could be "+cityname+" "+petname)
--- Day1/task.py
+++ Day1/task.py
@@ -1,2 +1,14 @@
-print("Hello world!")
+#Print Hello world!
+#print("Helloworld!")
+
+#String Concatenation
+#print("Hello"+" world!"+" Hello"+" Harsha!")
+
+#python input() function
+
+#print("Hello "+input("What is your name\n")+"!")
+
+username =input("What is your name\n")
+length=len(username)
+print(length)
 
